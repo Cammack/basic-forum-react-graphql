e7cfcc0dd32d30c110983d2b74c7dd742a9bab1d
Cammack <cmack525@gmail.com>
2021-12-30 23:02:40 -0700

    LeftMenu categories added + temp dataservice

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./LeftMenu.css";
 import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import Category from "../models/Category";
+import { getCategories } from "../services/DataService";
 
 const LeftMenu = () => {
     const { width } = useWindowDimensions();
 
+    const [categories, setCategories] = useState<JSX.Element>(<div>Left Menu</div>);
+
+    useEffect(() => {
+        getCategories()
+        .then((categories: Array<Category>) => {
+            const cats = categories.map((cat) => {
+                return <li key={cat.id}>{cat.name}</li>;
+            });
+            setCategories(<ul className="categories">{cats}</ul>);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, []);
+
     if(width <= 768) {
         return null;
     }
-    return <div className="leftmenu">Left Menu</div>
+    return <div className="leftmenu">{categories}</div>
 }
 
 export default LeftMenu;
